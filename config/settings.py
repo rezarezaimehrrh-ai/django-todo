7fb4b6f54bc727e109115d1df646833173926d09
ReZa <reza.rezaimehr.rh@gmail.com>
2026-06-17 19:37:37 -0400

    502

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-#ALLOWED_HOSTS = [ '*']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '188.40.16.3']
+ALLOWED_HOSTS = [ '*' ]
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '188.40.16.3']
 
 
 # Application definition

6d4f18038fea4cd511a58fd6ace81b7c70dd3f32
ReZa <reza.rezaimehr.rh@gmail.com>
2026-06-20 21:30:58 -0400

    set allowed hosts and csrf trusted origins

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ "*" ]
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '188.40.16.3']
+#ALLOWED_HOSTS = [ "*" ]
+ALLOWED_HOSTS = [' firstmytodo.runflare.run ']
+CSRF_TRUSTED_ORIGINS = [ 'https://firstmytodo.runflare.run' ]
 
 
 # Application definition

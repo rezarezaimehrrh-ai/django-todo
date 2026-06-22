6625f281b3867e53fa48f80e30ac874b9685b47d
ReZa <reza.rezaimehr.rh@gmail.com>
2026-06-21 22:04:35 -0400

    logout

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from django.views.decorators.cache import never_cache
+from django.contrib.auth import logout
  
 from .models import Task 
 
@@ -15,6 +16,7 @@ def add_task(request):
 
 
     if not user_id:
+         logout(request)
          return redirect("login")
     
 
